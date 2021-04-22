2fb745408e2c821842c35c6d1dabdcff7264621e
silverfoxpt <alterpt19@gmail.com>
2021-04-22 23:45:57 +0700

    Start GUI project. Testing phase.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
+#include "mainwindow.h"
+
 #include <QApplication>
-#include <QLabel>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QApplication a(argc, argv);
 
-    QLabel *label = new QLabel("Hello World");
-    label->setWindowTitle("New App");
-    label->resize(400, 400);
-    label->show();
+    MainWindow w;
+    w.show();
 
-    return app.exec();
+    return a.exec();
 }
